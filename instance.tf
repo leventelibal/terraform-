5c0bf81078339d2bd5022a32f65469d3915aef6e
leventelibal <leventelibal@gmail.com>
2019-07-28 18:14:48 -0400

    add _

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web1" {
     ami = "ami-0b898040803850657"
     instance_type = "t2.micro"
-    key_name = "${aws_key_pair.terraform_key.key_name}"
+    key_name = "${aws_key_pair.terraform.key_name}"
     security_groups = ["${aws_security_group.sec1.name}"]
     tags = {
         name = "dev"
