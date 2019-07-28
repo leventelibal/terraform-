b2b42388465a711def16ad1eb623e751b1360af8
leventelibal <leventelibal@gmail.com>
2019-07-28 18:08:47 -0400

    add .name

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web1" {
     ami = "ami-0b898040803850657"
     instance_type = "t2.micro"
     key_name = "${aws_key_pair.terraform-key.key_name}"
-    security_groups = ["${aws_security_group.sec1.id}"]
+    security_groups = ["${aws_security_group.sec1.name}"]
     tags = {
         name = "dev"
         dept = "it"
