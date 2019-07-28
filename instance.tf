f19dfa4e870d3d02e03010cb204b1b9fc9e61e91
leventelibal <leventelibal@gmail.com>
2019-07-28 17:51:43 -0400

    add tag

--- instance.tf
+++ instance.tf
@@ -2,5 +2,11 @@ resource "aws_instance" "web1" {
     ami = "ami-0b898040803850657"
     instance_type = "t2.micro"
     key_name = "terraform-key"
+    tags = {
+        name = "dev"
+        dept = "it"
+        group = "april"
+        created_by = "levent"
+    }
 
 }
