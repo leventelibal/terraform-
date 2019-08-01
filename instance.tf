3857e2c5386e9d807f0fdddb7491398dbbc6bfe0
leventelibal <leventelibal@gmail.com>
2019-07-31 20:34:13 -0400

    add user data

--- instance.tf
+++ instance.tf
@@ -3,6 +3,9 @@ resource "aws_instance" "web1" {
     instance_type = "t2.micro"
     key_name = "${aws_key_pair.terraform.key_name}"
     security_groups = ["${aws_security_group.sec1.name}"]
+
+    user_data= "${file("userdata.sh")}"
+    
     tags = {
         name = "dev"
         dept = "it"
