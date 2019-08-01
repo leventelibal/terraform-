05c405143fb3ecfa278a0c4f70842959ea515c3b
leventelibal <leventelibal@gmail.com>
2019-07-31 21:30:36 -0400

    add files

--- instance.tf
+++ instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web1" {
-    ami = "ami-0b898040803850657"
+    ami = "${var.ami}"
     instance_type = "t2.micro"
     key_name = "${aws_key_pair.terraform.key_name}"
     security_groups = ["${aws_security_group.sec1.name}"]
