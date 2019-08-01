84d5be8f41037f872d5554dae0631d03c281a40d
leventelibal <leventelibal@gmail.com>
2019-07-31 21:55:12 -0400

    add ss

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "jenkis2" {
   zone_id = "ZN0UYD2WE8CWJ"
-  name    = "dev.indyreliable.com"
+  name    = "dev2.indyreliable.com"
   type    = "A"
   ttl     = "300"
   records = ["${aws_eip.lb.public_ip}"]
